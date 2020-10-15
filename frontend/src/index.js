ee891fbd25823bc95ebac3d561343a28a1a5e0d4
unknown <alejolopez3310@gmail.com>
2020-10-14 20:50:08 -0500

    comienzo del desarrollo de pantallas

---
+++ frontend/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import App from './App';
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
