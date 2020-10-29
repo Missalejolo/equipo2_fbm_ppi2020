d0ae1c3cd09ae222ade664bf457ff285a6404bc0
kevinguzman1017152509 <kevinbogu@gmail.com>
2020-10-28 23:10:59 -0500

    nuevo aporte

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import Home from './pages/Home';
-import Iniciosesion from './pages/iniciosesion';
+import Iniciosesion from './pages/Iniciosesion';
 import Quienes from './pages/Quienes';
 import Registro from './pages/Registro';
 
