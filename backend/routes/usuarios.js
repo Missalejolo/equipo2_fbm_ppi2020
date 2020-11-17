995420a2ce8f7548f23d3841abcce3f8d4c03686
juanavel-t <juanavelasquez2603@gmail.com>
2020-11-17 18:10:26 -0500

    backend usuarios.js

--- backend/routes/usuarios.js
+++ backend/routes/usuarios.js
@@ -15,6 +15,34 @@ router.get('/',(req,res)=>{
     }// fin si
     })// fin query
     })// fin retorno de todos
+    router.post('/nuevo-usuario',(req,res)=>{
+        const {id,nombre,apellido,contraseña,correo,rol}=req.body;
+        
+        let alumno =[id,nombre,apellido,contraseña,correo,rol];
+        
+        let nuevoAlumno =`INSERT INTO tbl_usuarios(id,nombre,apellido,contraseña,correo,rol)
+        VALUES(?,?,?,?,?,?)`;
+        mysqlConnection.query(nuevoAlumno,alumno,(err,results,fields)=>{
+        if(err){
+           return console.error(err.message());
+        }else{
+           res.json({message:`usuario registrado`})
+        }
+        })});
+        router.post('/actualizar-usuario',(req,res)=>{
+            const {id,nombre,apellido,contraseña,correo,rol}=req.body;
+            
+            
+            mysqlConnection.query(`UPDATE tbl_usuarios SET id=?,nombre=?,apellido=?,contraseña=?,correo=?,rol=?`,
+            [id,nombre,apellido,contraseña,correo,rol],(err,rows,fields)=>{
+            if(!err){
+               
+               res.json({status:`usuario actualizado`});
+            }else{
+               console.log(err);
+              
+            }
+            })});
     
 
 module.exports =router;
